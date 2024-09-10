4f879d080e5b27f39139c2c17f8a3c13014f1172
Bezalel <bs0583251661@gmail.com>
2024-09-10 16:10:15 +0300

    Adding documentation and comments. Improving variables

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 def validate_Citibank_account(account: str, branch: str) -> bool:
-    multipliers = [3, 2, 7, 6, 5, 4, 3, 2]
-    len_account = 9
-    len_branch = 3
-    if account.isdigit() and len(account) == len_account and len(branch) == len_branch and branch.isdigit():
+    """
+       Validates a Citibank account number based on a checksum algorithm.
+
+       The account number must be 9 digits long, and the branch number must be 3 digits long.
+
+       Args:
+       account (str): A 9-digit string representing the account number.
+       branch (str): A 3-digit string representing the branch number.
+
+       Returns:
+       bool: Returns True if the account number is valid according to the checksum algorithm.
+             Returns False if the account number or branch number is invalid..
+       """
+    MULTIPLIERS = [3, 2, 7, 6, 5, 4, 3, 2]
+    LEN_ACCOUNT = 9
+    LEN_BRANCH = 3
+    MODULO = 11
+    # Validate that the account number and branch number are digits of correct lengths
+    if account.isdigit() and len(account) == LEN_ACCOUNT and len(branch) == LEN_BRANCH and branch.isdigit():
         verification_digit = account[-1]
-        total_sum = sum(map(lambda x, y: int(x) * y, account[:8], multipliers))
-        result = total_sum % 11
-        check_digit = 11 - result if result != 0 else 0
+        num_to_check = account[:8]
+        # Calculate the sum of the first 8 digits multiplied by their corresponding multipliers
+        total_sum = sum(map(lambda x, y: int(x) * y, num_to_check, MULTIPLIERS))
+
+        result = total_sum % MODULO
+
+        check_digit = MODULO - result if result != 0 else 0
         if check_digit == int(verification_digit):
             return True
     return False
-
