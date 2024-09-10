e4960dc79093ca4adc0aeef9181349956e66f479
Bezalel <bs0583251661@gmail.com>
2024-09-10 15:26:07 +0300

    finish function

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
-# This is a sample Python script.
+def validate_Citibank_account(account: str, branch: str) -> bool:
+    multipliers = [3, 2, 7, 6, 5, 4, 3, 2]
+    len_account = 9
+    len_branch = 3
+    if account.isdigit() and len(account) == len_account and len(branch) == len_branch and branch.isdigit():
+        verification_digit = account[-1]
+        total_sum = sum(map(lambda x, y: int(x) * y, account[:8], multipliers))
+        result = total_sum % 11
+        check_digit = 11 - result if result != 0 else 0
+        if check_digit == int(verification_digit):
+            return True
+    return False
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-    print("wow")
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
