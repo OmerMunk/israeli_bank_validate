af8d813b70a36fd4d18bc09a4338975b9a7fdfa1
RafiGedge <g0534183790@gmail.com>
2024-09-10 18:56:29 +0300

    More detailed documentation

--- Bank.py
+++ Bank.py
@@ -1,24 +1,41 @@
-def validate_the_international_account(account_number: str,  branch_number: str) -> bool:
+def validate_the_international_account(account_number: str, branch_number: str) -> bool:
     """
     Validate an international account number using a weighted sum calculation.
     If the account is not valid for a 9-digit check, it checks the last 6 digits.
-    If both checks fail, it validates using the the_soldiers_treasure account validation.
-    """
-    max_len = 9
-    account_n = f"{account_number:0>{max_len}}"
+    If both checks fail, it validates using the soldiers treasure validation.
 
-    if not max_len >= len(account_number) >= 2:
+    :param account_number: The account number to validate, expected to be between 2 and 6 digits long.
+    :param branch_number: The branch number associated with the account, used by the soldiers treasure validation.
+    :return: :return: True if the account number is valid, false otherwise.
+    """
+
+    max_len = 9
+    min_len = 2
+
+    if not max_len >= len(account_number) >= min_len:
         return False
 
-    def check(an: str, ml: int = max_len) -> bool:
-        list_of_an = map(int, an)
-        list_of_keys = range(ml, 0, -1)
-        return sum(n * k for n, k in zip(list_of_an, list_of_keys)) % 11 in (0, 6)
+    list_of_numbers = list(map(int, f"{account_number:0>{max_len}}"))
 
-    if check(account_n):
+    if check(list_of_numbers):
         return True
 
-    if check(account_n[-6:], 6):
+    if check(list_of_numbers[-6:]):
         return True
 
     return validate_the_soldiers_treasure_account(account_number, branch_number)
+
+
+def check(list_of_numbers: list) -> bool:
+    """
+    Helper function for validating an account number using a weighted sum calculation.
+
+    This function takes an account number as a list of digits and performs a calculation
+    where each digit is multiplied by a descending weight starting from the length of the list.
+    The sum of these products is then checked to see if it's divisible by 11 with a remainder of 0 or 6.
+
+    :param list_of_numbers: A list of integers representing the digits of the account number.
+    :return: True if the account number is valid, false otherwise.
+    """
+    list_of_keys = range(len(list_of_numbers), 0, -1)
+    return sum(n * k for n, k in zip(list_of_numbers, list_of_keys)) % 11 in (0, 6)
