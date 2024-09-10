89924985cda059ab4583583fb3ec7c2f0dbc2990
Yoni <yonie3921@gmail.com>
2024-09-10 17:39:48 +0300

    remove the prints

--- poalim_bank.py
+++ poalim_bank.py
@@ -5,19 +5,34 @@ def validate_the_operatives_account(account_number: str, branch_number: str):
     :param branch_number: string of the branch number
     :return: True if successful, False otherwise
     '''
+
+    # check if the length of the acoount number and branch number are correct
+    # otherwise adding zeros
+    ACOUNT_DIGITS = 6
+    BRANCH_DIGITS = 3
+
+    if len(account_number) < ACOUNT_DIGITS:
+        account_number = f"{account_number:0 > ACOUNT_DIGITS}"
+        print(account_number)
+    if len(branch_number) < BRANCH_DIGITS:
+        branch_number = f"{branch_number:0 > BRANCH_DIGITS}"
+        print(branch_number)
+
     result: int= 0
     NUMBER_TO_DIVIDE: int = 11
     calculation: str = branch_number + account_number
-    for i in range(1, len(calculation)+1) :
-        result += i * int(calculation[-i])
-    is_legal: int = result % NUMBER_TO_DIVIDE
 
-    if is_legal % 2 == 0 and is_legal <= round(NUMBER_TO_DIVIDE / 2) or is_legal == 0:
+    # calculate the sum of all the account and branch numbers
+    for i in range(1, len(calculation) + 1) :
+        result += i * int(calculation[-i])
+    remainder: int = result % NUMBER_TO_DIVIDE
+
+    # check if the remainder is a legal number
+    if remainder % 2 == 0 and remainder <= round(NUMBER_TO_DIVIDE / 2) or remainder == 0:
         return True
     return False
 
 
-print(validate_the_operatives_account('065257', "698"))
 
 
 
