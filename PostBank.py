2f9cd109dbf288759cc96f70f74438f3f2a0ab3c
matan <jerbimatan@gmail.com>
2024-09-10 16:04:12 +0300

    fix variables names

--- PostBank.py
+++ PostBank.py
@@ -1,15 +1,17 @@
 # 10 . בנק הדואר
 def validate_The_Mail_account(acount : str):
     # 059121900
+    SIZE_OF_ACOUNT = len(acount)
+    LIMIT_SIZE = 9
     try:
-        if not acount.isdigit() or len(acount) > 9:
+        if not acount.isdigit() or len(acount) > LIMIT_SIZE:
             raise ValueError("שגיאה: מספר החשבון חייב להכיל רק ספרות ואורכו לא יכול לעלות על 9 ספרות.")
 
-        acount_number: str = acount.zfill(9)
-        sum = 0
-        for i in range(0, 9):
-            sum += (9 - i) * int(acount_number[i])
-        if sum % 10 == 0:
+        acount_number: str = acount.zfill(SIZE_OF_ACOUNT)
+        calculate_multply = 0
+        for i in range(0, SIZE_OF_ACOUNT):
+            calculate_multply += (SIZE_OF_ACOUNT - i) * int(acount_number[i])
+        if calculate_multply % 10 == 0:
             return True
         else:
             return False
@@ -17,4 +19,4 @@ def validate_The_Mail_account(acount : str):
         print(e)
         return False
 
-print(validate_The_Mail_account("059121900"))
+print(validate_The_Mail_account("00059121900"))
