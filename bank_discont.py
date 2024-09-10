777a4ef8efaa0e3013bfcdc7bcf1bc207e65bd84
NaftaliRisemberg <n9742686@gmail.com>
2024-09-10 16:30:43 +0300

    I output the result to another variable and fixed the error in the condition that checks the result received, order the constant variables at the beginning of the function

--- bank_discont.py
+++ bank_discont.py
@@ -1,12 +1,16 @@
 # 4. קבוצת דיסקונט
-def validateDiscount_account(account_number: str, branch_number: str):
-    if len(account_number) != 9:
-        return False
-    list_account_number = [int(num) for num in account_number]
+def validateDiscount_account(account_number: str, branch_number: str) -> bool:
+    VALID_LENGTH = 9
     NUM_MULTIPLITED_INDEX = 1
     MULTIPLITER_NUMBER_INDEX = 0
+    if len(account_number) != VALID_LENGTH:
+        return False
+    list_account_number = [int(num) for num in account_number]
     sum_multiple_numbers = sum(list(map(lambda x: x[NUM_MULTIPLITED_INDEX] * x[MULTIPLITER_NUMBER_INDEX], enumerate(list_account_number, start=NUM_MULTIPLITED_INDEX))))
-    if sum_multiple_numbers / 11 == 0 or 2 or 4:
+    final_result = sum_multiple_numbers / 11
+    valid_result = [0, 2, 4]
+    if final_result in valid_result:
         return True
 
 print(validateDiscount_account("111111111", "123456"))
+
