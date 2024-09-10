d018b7fb0b066f2ec29881c0b9728a21eed107b3
Eytan zichel <eytanzichel@gmail.com>
2024-09-10 17:24:54 +0300

    Insert documentation inside the function

--- Multiply_by_range.py
+++ Multiply_by_range.py
@@ -1,10 +1,9 @@
-
-"""
-The function receives a string that consists of the branch number + account number,
-and the range of numbers to be multiplied by.
-note that the range does not include the last digit so you need to add a number!
-"""
 def multiply_by_range(account_str, rng):
-    zip1 =  zip(account_str, reversed(rng))
-    res = sum(map(lambda  x: int(x[0]) * int(x[1]), zip1))
+    """
+    The function receives a string that consists of the branch number + account number,
+    and the range of numbers to be multiplied by.
+    note that the range does not include the last digit so you need to add a number!
+    """
+    num_multiplier_tuples =  zip(account_str, reversed(rng))
+    res = sum(map(lambda  x: int(x[0]) * int(x[1]), num_multiplier_tuples))
     return (res)
